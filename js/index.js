db68d74bc318cabc897ec2eccbbde99efa82fe81
Joaquin3312 <87045994+Joaquin3312@users.noreply.github.com>
2024-10-18 20:47:20 -0400

    Agregando los primeros pasos para la gestión de usuarios

---
+++ js/index.js
@@ -0,0 +1,9 @@
+console.log("Hola este es un mensaje en consola");
+
+//Aquí irá la lógica para la gestión de usuarios por una parte
+
+const accederBoton = document.getElementById("login")
+
+accederBoton.addEventListener("click",()=>{
+    alert("Seguimos trabajando en eso! disculpas por las molestias")
+})
